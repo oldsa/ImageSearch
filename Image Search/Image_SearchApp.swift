8bd8ccacee22365b45e63d4afb034767f2c53b2b
Anthony Olds <anthony.olds@gmail.com>
2022-01-26 16:37:14 -0700

    Initial Commit

---
+++ Image Search/Image_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_SearchApp.swift
+//  Image Search
+//
+//  Created by Anthony Olds on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Image_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Image SearchUITests/Image_SearchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Image_SearchUITestsLaunchTests.swift
+//  Image SearchUITests
+//
+//  Created by Anthony Olds on 1/26/22.
+//
+
+import XCTest
+
+class Image_SearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
